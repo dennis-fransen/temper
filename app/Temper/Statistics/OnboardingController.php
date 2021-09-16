36e841d192e88e37e1d82dd6db6f2901324758eb
Dennis Fransen <d.fransen@yonego.nl>
2021-09-16 09:28:27 +0200

    Cleanup sort function, return more usefull error, basic filter check on col allowance

--- app/Temper/Statistics/OnboardingController.php
+++ app/Temper/Statistics/OnboardingController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Temper\Statistics;
 
+use App\Temper\Statistics\Collections\OnboardingCollection;
 use App\Temper\Statistics\Requests\OnboardingStatisticsListRequest;
 use App\Temper\Statistics\Resources\OnboardingResourceCollection;
 use App\Temper\Statistics\Services\OnboardingDataCollector;
@@ -15,6 +16,8 @@ class OnboardingController
 {
     private OnboardingDataCollector $onboardingDataCollector;
 
+    public const FILTER_FIELDS = ['created_at', 'onboarding_perentage', 'user_id', 'count_applications', 'count_accepted_applications'];
+
     public function __construct(OnboardingDataCollector $onboardingDataCollector)
     {
         $this->onboardingDataCollector = $onboardingDataCollector;
@@ -22,6 +25,7 @@ class OnboardingController
 
     /**
      * @throws FileNotFoundException
+     * @throws \JsonException
      */
     public function index(OnboardingStatisticsListRequest $request): JsonResponse
     {
@@ -41,14 +45,64 @@ class OnboardingController
 //        ]);
 
         // If we have a csv without header, we can build our own template
-        $fieldTemplate = ['user_id', 'created_at', 'onboarding_perentage', 'count_applications', 'count_accepted_applications'];
+        $fieldTemplate = [
+            'user_id',
+            'created_at',
+            'onboarding_perentage',
+            'count_applications',
+            'count_accepted_applications'
+        ];
+
         $data = $this->onboardingDataCollector->gatherData(['json', 'csv'], [
             'csv_skip_header' => true,
             'template' => $fieldTemplate,
         ]);
 
-        $data = $this->applyFilters($request, $data);
+        try {
+            $data = $this->applyFilters($request, $data);
+            $data = $this->applySorters($request, $data);
+        } catch (\Exception $ex) {
+            return new JsonResponse([
+                'error' => [
+                    'message' => $ex->getMessage(),
+                ],
+            ], 422);
+        }
 
+        return new JsonResponse(new OnboardingResourceCollection($data));
+    }
+
+    /**
+     * @throws \Exception
+     */
+    protected function applyFilters(OnboardingStatisticsListRequest $request, mixed $data): mixed
+    {
+
+        if ($request->get('filter')) {
+            foreach ($request->get('filter') as $filterCol => $parameter) {
+
+                // Check if filter is valid
+                if (!in_array($filterCol, self::FILTER_FIELDS)) {
+                    throw new \Exception('Invalid filter, use one off:' . implode(', ', self::FILTER_FIELDS));
+                }
+
+                // Make created at a carbon object to cleanly compare it to our stored created_at value
+                if ('created_at' === $filterCol) {
+                    $parameter = new Carbon($parameter);
+                }
+
+                $data = $data->where($filterCol, $parameter);
+            }
+        }
+
+        return $data;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    protected function applySorters(OnboardingStatisticsListRequest $request, OnboardingCollection $data): OnboardingCollection
+    {
         if ($request->get('sort')) {
             foreach ($request->get('sort') as $sortCol => $direction) {
                 if ('asc' === $direction) {
@@ -56,28 +110,8 @@ class OnboardingController
                 } elseif ('desc' === $direction) {
                     $data = $data->sortByDesc($sortCol);
                 } else {
-                    return new JsonResponse([
-                        'error' => [
-                            'message' => 'invalid sort option',
-                        ],
-                    ], 422);
+                    throw new \Exception('Invalid sort direction given, please use "asc" or "desc"');
                 }
-            }
-        }
-
-        return new JsonResponse(new OnboardingResourceCollection($data));
-    }
-
-    protected function applyFilters(OnboardingStatisticsListRequest $request, mixed $data): mixed
-    {
-        if ($request->get('filter')) {
-            foreach ($request->get('filter') as $filterCol => $parameter) {
-                // Make created at a carbon object to cleanly compare it to our stored created_at value
-                if ('created_at' === $filterCol) {
-                    $parameter = new Carbon($parameter);
-                }
-
-                $data = $data->where($filterCol, $parameter);
             }
         }
 
